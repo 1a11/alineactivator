2649a0fad1006ad91b5bb6f62c57948d877643f9
Vladimir <48794941+1a11@users.noreply.github.com>
2019-07-19 16:18:51 +0400

    Add files via upload

--- flask_app.py
+++ flask_app.py
@@ -8,14 +8,16 @@ app = Flask(__name__)
 @app.route('/activate/', methods=['POST','GET'])
 def accept():
     db = dat.database('prod.db')
-    akey = request.args.get("akey")
-    skey = request.args.get("skey")
-    if db.get_apikey(akey) == []:
-        return('500')
+    data = request.get_json()
+    data = dict(data)
+    skey = data['skey']
+    akey = data['akey']
+    skey = db.get_settings(skey)[0][1]
+    akey = db.get_apikey(akey)[0][0]
+    print(akey,skey)
+    if akey:
+        return(skey)
     else:
-        if db.get_settings(skey) == []:
-            return('500')
-        else:
-            return (db.get_settings(skey)[1])
+        return('500')
 
-
+app.run()
